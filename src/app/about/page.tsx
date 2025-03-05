733a7d4dfa269a121ac7210501c758af48e3e9e3
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-03-05 20:21:21 +0200

    feat: :sparkles: completing the values of the company

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image"
+import Image from "next/image";
+import { Award, ShieldCheck, HandCoins, Handshake } from "lucide-react";
 
 export default function About () {
   return (
@@ -58,6 +59,49 @@ export default function About () {
         <div className="flex flex-col gap-7">
           <p className="font-semibold text-2xl">Leadership & Expertise</p>
           <p className="text-lg text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+      </div>
+      {/* Values section */}
+      <div className="flex flex-col gap-12 relative bg-white text-black p-10">
+      <div className="flex flex-col gap-4 items-center">
+          {/* Icon area */}
+          <div className="bg-blue-500 rounded-full p-5">
+            <Award size={55} color="white" />
+          </div>
+          <p className="font-semibold text-xl">Excellence</p>
+          <p className="text-center text-gray-500 max-w-xs">
+            Lorem ipsum dolor sit amet, consectetur adipiscing
+          </p>
+        </div>
+        <div className="flex flex-col gap-4 items-center">
+          {/* Icon area */}
+          <div className="bg-blue-500 rounded-full p-5">
+            <ShieldCheck size={55} color="white" />
+          </div>
+          <p className="font-semibold text-xl">Reliability</p>
+          <p className="text-center text-gray-500 max-w-xs">
+            Lorem ipsum dolor sit amet, consectetur adipiscing
+          </p>
+        </div>
+        <div className="flex flex-col gap-4 items-center">
+          {/* Icon area */}
+          <div className="bg-blue-500 rounded-full p-5">
+            <HandCoins size={55} color="white" />
+          </div>
+          <p className="font-semibold text-xl">Affordability</p>
+          <p className="text-center text-gray-500 max-w-xs">
+            Lorem ipsum dolor sit amet, consectetur adipiscing
+          </p>
+        </div>
+        <div className="flex flex-col gap-4 items-center">
+          {/* Icon area */}
+          <div className="bg-blue-500 rounded-full p-5">
+            <Handshake size={55} color="white" />
+          </div>
+          <p className="font-semibold text-xl">Loyalty</p>
+          <p className="text-center text-gray-500 max-w-xs">
+            Lorem ipsum dolor sit amet, consectetur adipiscing
+          </p>
         </div>
       </div>
     </div>
